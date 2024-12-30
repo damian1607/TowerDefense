994b7f45523a3877557f741f6edf4ae5374cb478
Damian <damianchwalek05@gmail.com>
2024-12-31 00:33:01 +0100

    Codekommentare hinzugefügt

--- scripts/SceneHandler.gd
+++ scripts/SceneHandler.gd
@@ -6,8 +6,10 @@ func _ready() -> void:
 	get_node("MainMenu/MarginContainer/VBoxContainer/Start").connect("pressed", Callable(self, "onStartPressed"))
 	get_node("MainMenu/MarginContainer/VBoxContainer/Quit").connect("pressed", Callable(self, "onQuitPressed"))
 
+# Start Knopf gedrückt
 func onStartPressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/GameScene.tscn")
-	
+
+# Quit Knopf gedrückt
 func onQuitPressed() -> void:
 	get_tree().quit()
